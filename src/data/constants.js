680404f41f1e397721636c95fd418d3a6164e9d8
Ihor <vitrikushihor0101@gmail.com>
2024-03-06 13:07:15 +0200

    add new cv

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
 		 "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
 	github: "https://github.com/VitrikushIhor",
 	resume:
-		 "https://drive.google.com/file/d/1MJ4sSLXMize7DdPmxl-ucqra29-vemep/view",
+		 "https://drive.google.com/file/d/1GSIAcV6ULL1jHWnBrguq6cLkh5mm47VK/view",
 	linkedin: "https://www.linkedin.com/in/ihor-vitrykush-654b73222/",
 	telegram: "https://t.me/Nyarlathoted",
 	insta: "https://www.instagram.com/nyarlath0ted/",
