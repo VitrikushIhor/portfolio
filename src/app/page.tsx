17381276c4d81ca9e9e73e917c7b620bfefe463f
ihor <ihorvitrikush0101@gmail.com>
2024-07-21 22:57:44 +0300

    fixed problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Wrapper from '@/components/Wrapper/Wrapper';
 import axios from 'axios';
 import {IResponse} from '@/data/constants';
+import {fetchDataCart} from '@/service/rtkApi';
 
 
 export default async function Home() {
 
-
-	const {data} = await axios.get<IResponse>(`${process.env.NEXT_PUBLIC_API_URL}/getInfo`)
+	const data = await fetchDataCart('/getInfo')
 	return (
 		 <Wrapper data={data}/>
 	);
--- src/service/rtkApi.ts
+++ src/service/rtkApi.ts
@@ -9,3 +9,15 @@ export const rtkApi = createApi({
 
 	endpoints: () => ({}),
 });
+
+export const fetchDataCart = async (url: string, options?: RequestInit) => {
+	const baseURL = process.env.NEXT_PUBLIC_API_URL;
+	const response = await fetch(`${baseURL}${url}`, {
+		credentials: 'include',
+		...options,
+	});
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	return response.json();
+};
