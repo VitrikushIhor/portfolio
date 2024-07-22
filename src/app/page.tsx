081c77084ac812240a7672d68a084584126a7501
Ihor <vitrikushihor0101@gmail.com>
2024-07-22 21:15:35 +0300

    try fixed deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Wrapper from '@/components/Wrapper/Wrapper';
-import axios from 'axios';
-import {IResponse} from '@/data/constants';
-import {fetchDataCart} from '@/service/rtkApi';
-
-
 export default async function Home() {
 
-	const data = await fetchDataCart('/getInfo')
+	// const data = await fetchDataCart('/getInfo')
 	return (
-		 <Wrapper data={data}/>
+		<div>123</div>
+		 // <Wrapper data={data}/>
 	);
 }
